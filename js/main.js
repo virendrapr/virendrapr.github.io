d2bde0f962140de95be571d6ac3ced125abed7fb
Virendra Pandey <57798999+virendrapr@users.noreply.github.com>
2020-01-27 15:00:34 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -34,7 +34,13 @@
 			minlength: "Please enter valid mobile number (10 - digits)"
 		},
 	},
-	submitHandler: function (form) {}
+	submitHandler: function (form) {
+	      ev.preventDefault();
+	      var data = new FormData(form);
+	      ajax(form.method, form.action, data, success, error);
+
+	      return false;
+	}
 
 });
 
@@ -304,4 +310,37 @@
 
 
 })(jQuery);
+// get the form elements defined in your form HTML above
+    
+    var form = document.getElementById("conatctForm");
+    var button = document.getElementById("my-form-button");
+    var status = document.getElementById("my-form-status");
 
+    // Success and Error functions for after the form is submitted
+    
+    function success() {
+      form.reset();
+      button.style = "display: none ";
+      status.innerHTML = "Thanks!";
+    }
+
+    function error() {
+      status.innerHTML = "Oops! There was a problem.";
+    }
+
+  // helper function for sending an AJAX request
+
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
