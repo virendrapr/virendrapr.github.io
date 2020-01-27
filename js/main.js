282227d6ae66817d8b729787ac6bb49827407f3e
Virendra Pandey <57798999+virendrapr@users.noreply.github.com>
2020-01-27 23:10:04 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -296,13 +296,11 @@
   });
 
   $('.popup-youtube').magnificPopup({
-    disableOn: 700,
     type: 'iframe',
     mainClass: 'mfp-fade',
     removalDelay: 160,
-    preloader: false,
-
-    fixedContentPos: false
+    preloader: true,
+    fixedContentPos: true
   });
 
  $('.popup-youtube').click(function(){
